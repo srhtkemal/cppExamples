4289f6767cd8820926da553c2146cae16e301396
Serhat Kemal <97290537+srhtkemal@users.noreply.github.com>
2022-11-05 12:23:04 +0300

    Add files via upload

---
+++ encapsulation.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+	class Employee {
+		private:
+			string name;
+			int age;
+		public:
+			void setName(string pubName) {
+				 name= pubName;
+			}
+			void setAge(int pubAge) {
+				if (pubAge > 0 && pubAge < 100)
+					age = pubAge;
+				else
+					cout << "Employee's age is probably wrong, please check"<< endl;
+			}
+			string getName() {
+				return name;
+			}
+			int getAge() {
+				return age;
+			}
+
+	};
+	int main() {
+		Employee employee;
+		employee.setName("Serhat Kemal");
+		employee.setAge(19);
+
+		cout << employee.getName() << endl;
+		cout << employee.getAge() << endl;
+		return 0;
+	}
